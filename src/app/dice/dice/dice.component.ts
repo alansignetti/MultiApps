11f85efbc47d7ce8ce6bf0c2a023f833e3009b6a
asignetti <asignetti@expand.com.uy>
2022-07-25 17:18:58 -0300

    DiceApp img

--- src/app/dice/dice/dice.component.ts
+++ src/app/dice/dice/dice.component.ts
@@ -18,8 +18,9 @@ export class DiceComponent implements OnInit {
   public rollDice() {
     this.num1 = Math.round(Math.random() * 5) + 1;
     this.num2 = Math.round(Math.random() * 5) + 1;
-    this.leftDice = '../../../assets/dice/dice' + this.num1 + '.png';
-    this.rightDice = '../../../assets/dice/dice' + this.num2 + '.png';
+    this.leftDice = 'src/assets/dice/dice' + this.num1 + '.png';
+    this.rightDice = 'src/assets/dice/dice' + this.num2 + '.png';
+
     this.disableButton();
   }
   public disableButton() {
